e40a53704368e5da2d48ec8c265d99303d5517c3
andryyy <andre.peters@debinux.de>
2021-08-21 09:52:36 +0200

    Sat 21 Aug 2021 09:52:36 AM CEST

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,7 +7,7 @@ SITEURL = 'https://debinux.de'
 SITETITLE = "debinux.de"
 SITESUBTITLE = "an admins notepad"
 FAVICON = SITEURL + "/images/favicon.png"
-SITELOGO = SITEURL + "/images/logo.png"
+SITELOGO = SITEURL + "/images/logo.svg"
 PORT = 8000
 
 STATIC_PATHS = ["extra/custom.css", "extra/keys", "images/", "extra/robots.txt"]
