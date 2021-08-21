016accf5323d2c8bf43b5f890252ab0acbd20ea7
andryyy <andre.peters@debinux.de>
2021-08-21 08:27:05 +0200

    Sat 21 Aug 2021 08:27:05 AM CEST

--- pelicanconf.py
+++ pelicanconf.py
@@ -23,11 +23,9 @@ PATH = 'content'
 TIMEZONE = 'Europe/Berlin'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+FEED_DOMAIN = SITEURL
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 # Blogroll
 LINKS = (
